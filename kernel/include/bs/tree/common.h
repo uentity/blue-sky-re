7556448896f17c2dc11a978fd27f3cb7bd0dbcb4
Alexander Gagarin @uentity <unientity@gmail.com>
2021-07-05 15:08:06 +0500

    tree: minor refactoring of `deref_path_impl()`
    Switch to using `std::string_view`, move `to_string(lids_v)` from
    context impl to public API.

--- kernel/include/bs/tree/common.h
+++ kernel/include/bs/tree/common.h
@@ -74,6 +74,7 @@ enum class TreeOpts : std::uint32_t {
 
 /// link's unique ID type
 using lid_type = uuid;
+BS_API auto to_string(const lids_v& path, bool as_absolute = true) -> std::string;
 
 /// forward declare major types
 class link;
